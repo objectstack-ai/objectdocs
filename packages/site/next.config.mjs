cfb162d58d6c3a9cbbd509bcef82205eb89ef202
Jack <zhuangjianguo@gmail.com>
2026-01-18 23:42:10 +0800

    chore: add transpilePackages to next.config.mjs

--- packages/site/next.config.mjs
+++ packages/site/next.config.mjs
@@ -15,6 +15,7 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: '.next',
   images: { unoptimized: true },
+  transpilePackages: ['@objectdocs/site'],
 };
 
 export default withMDX(nextConfig);
